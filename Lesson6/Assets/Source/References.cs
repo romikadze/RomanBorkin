ba3ed7b9f9a384a745b481d6a612fe6298d1d1a6
Roman <romik9592@gmail.com>
2023-11-08 21:59:14 +0300

    Lesson6

---
+++ Lesson6/Assets/Source/References.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class References : MonoBehaviour
+{
+    private int _id = 1;
+    private string _name = "User";
+    private Data _data = new Data();
+
+    private void Start()
+    {
+        int newId = _id;
+        _id = 5;
+        Debug.Log("ID: " + _id);
+        Debug.Log("new ID: " + newId);
+
+        //???????
+        string newName = _name;
+        _name = "User123";
+        Debug.Log("Name: " + _name);
+        Debug.Log("new Name: " + newName);
+        //???????
+
+        _data.Id = _id;
+
+        Data newData = _data;
+        _data.Id = 10;
+        Debug.Log("Data ID: " + _data.Id);
+        Debug.Log("newData ID: " + newData.Id);
+
+        CreateLogin(newId, newName, out string login);
+        Debug.Log("Login " + login);
+    }
+
+    private void CreateLogin(int id, string name, out string login)
+    {
+        login = name + id;
+    }
+}
+
+class Data
+{
+    public int Id { get; set; }
+}
